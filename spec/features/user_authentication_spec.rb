47e6ccba1cde2930d09bef7ce42b628bed0991ad
njgheorghita <njgheorghita@email.wm.edu>
2017-01-31 13:54:38 -0700

    implement user logout feature test

--- spec/features/user_authentication_spec.rb
+++ spec/features/user_authentication_spec.rb
@@ -12,4 +12,17 @@ describe 'User logs in via Facebook Oauth' do
     expect(page).to     have_content("Write A Letter")
     expect(page).not_to have_content("Login with Facebook")
   end
+
+  scenario 'and can logout' do 
+    user = User.create(name: "Nick", uid: "1234")
+    
+    page.set_rack_session(user_id: user.uid)
+
+    visit root_path
+
+    expect(page).to have_content("Write A Letter")
+    click_on "Sign Out"
+
+    expect(page).to have_content("Login with Facebook")
+  end
 end
