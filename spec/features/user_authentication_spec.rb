63b351569ba3d57c1033fe4c457f17ce6e9b1409
njgheorghita <njgheorghita@email.wm.edu>
2017-01-31 13:51:51 -0700

    pass user authentication

--- spec/features/user_authentication_spec.rb
+++ spec/features/user_authentication_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 describe 'User logs in via Facebook Oauth' do 
   scenario 'and can see option to create letter' do 
-    user = User.create(name: "Nick")
+    user = User.create(name: "Nick", uid: "1234")
     
-    page.set_rack_session(user_id: user.id)
+    page.set_rack_session(user_id: user.uid)
 
     visit root_path
-
+    
     expect(page).to     have_content("Sign Out")
     expect(page).to     have_content("Write A Letter")
     expect(page).not_to have_content("Login with Facebook")
