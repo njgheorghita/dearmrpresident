3301f6af13ff2da13b00d20a418967166bc085f0
njgheorghita <njgheorghita@email.wm.edu>
2017-02-01 20:26:49 -0700

    style front page

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,5 +1,10 @@
 class Purchase < ApplicationRecord
 
+  STATES = ["AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS",
+            "KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY",
+            "NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV",
+            "WI","WY"]
+
   def order_letter(lob, politician)
     lob.letters.create(
       description:        self.description, 
