8e63b9d0811c0129899b3385da760daa25712e63
njgheorghita <njgheorghita@email.wm.edu>
2017-02-19 20:59:49 -0700

    Add character limit to letter_body

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -2,11 +2,17 @@ class LettersController < ApplicationController
   
   def new
     @letter = Letter.new
+    @maximum_length = Letter.validators_on( :letter_body ).first.options[:maximum]
   end
 
   def create
-    Letter.create(letter_params)
-    redirect_to root_path
+    letter = Letter.new(letter_params)
+    if letter.save
+      redirect_to root_path
+    else 
+      flash[:danger] = "Please make sure letter is less than 500 chars"
+      redirect_to new_letter_path
+    end
   end
 
   def destroy
--- app/models/letter.rb
+++ app/models/letter.rb
@@ -1,3 +1,4 @@
 class Letter < ApplicationRecord
+  validates :letter_body, length: { maximum: 450 }
   has_one :purchase
 end
